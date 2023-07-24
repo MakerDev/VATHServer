6dff74068c4a40a90f6e640b16ebbe2fd799ef8b
YujinShin <raki2001@naver.com>
2023-07-24 23:21:59 +0900

    IP address display is added.

--- VathServer/ViewModels/SettingPageViewModel.cs
+++ VathServer/ViewModels/SettingPageViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,51 @@ namespace VathServer.ViewModels
         private double _imageSizeInCm = 5;
         [ObservableProperty]
         private double _contrastValue = 1.0;
+        [ObservableProperty]
+        private string _ipAddress = "";
+        public SettingPageViewModel()
+        {
+#if WINDOWS
+            GetIpAddress();
+#endif  
+        }
+
+        private void GetIpAddress()
+        {
+            // Get all network interfaces on the system
+            NetworkInterface[] networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+
+            // Loop through each network interface
+            foreach (NetworkInterface networkInterface in networkInterfaces)
+            {
+                // Check if the interface is operational and not a loopback or tunnel interface
+                if (networkInterface.OperationalStatus == OperationalStatus.Up &&
+                    networkInterface.NetworkInterfaceType != NetworkInterfaceType.Loopback &&
+                    networkInterface.NetworkInterfaceType != NetworkInterfaceType.Tunnel)
+                {
+                    // Get the IP properties of the interface
+                    IPInterfaceProperties ipProperties = networkInterface.GetIPProperties();
+
+                    // Get the collection of unicast IP addresses assigned to the interface
+                    UnicastIPAddressInformationCollection ipAddresses = ipProperties.UnicastAddresses;
+
+                    // Loop through each IP address and display it
+                    foreach (UnicastIPAddressInformation ipAddress in ipAddresses)
+                    {
+                        // Only consider IPv4 addresses
+                        if (ipAddress.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork 
+                                && ipAddress.IPv4Mask.ToString().Split('.')[2] == "255")
+                        {
+                            Console.WriteLine($"IPv4 Address: {ipAddress.Address}");
+                            IpAddress = ipAddress.Address.ToString();
+
+                            //Take the first address.
+                            return;
+                        }
+                    }
+                }
+            }
+        }
 
         [RelayCommand]
         private async Task StartSessionAsync()
